307d0a495104212383b163e1106b253ec678eb6d
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2020-11-04 18:03:09 -0600

    Cambio de interfas
    se restablecio el ligin ye ingreso al sistema

--- principal.php
+++ principal.php
@@ -10,14 +10,26 @@ if (!isset($usuario)) {
   <head>
     <meta charset="utf-8">
     <title> Inicio de Sistema | 505 and 506</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/simple-sidebar.css">
     <script src="js/jquery-3.5.1.min.js"></script>
   </head>
   <body>
-    <?php include 'main/nabvar.php';?>
-     <div class="container">
-        <h4 class="text-center"> Sistema Gestor de Base de datos </h4>
-     </div>
+    <?php include 'main/sidebar.php';?>
+    <div id="page-content-wrapper">
+      <?php include 'main/navbar_principal.php';?>
+      <h4 class="text-center display-3"> Dashboard</h4>
+
+
+    </div>
   <script src="js/bootstrap.min.js"></script>
+  <script>
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+  </script>
   </body>
 </html>
