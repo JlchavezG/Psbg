fe07f8727579769d3b44b58f7a199a9196032841
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2020-12-28 10:44:10 -0600

    cambio en notificaciones
    cambio en notificaciones. sistemas

--- main/notificaciones.php
+++ main/notificaciones.php
@@ -25,51 +25,61 @@ if ($notificacion > 0) {
 }
  ?>
 <div class="container py-4">
-  <div class="card shadow-lg p-3 mb-5 rounded">
-      <div class="container">
-         <div class="row">
-           <div class="col-sm-12 col-md-12 col-lg-12 py-4">
-              <p class="text-muted text-center">Genera una nueva notificación</p>
+     <div class="row py-3">
+         <div class="col">
+
+         </div>
+         <div class="col">
+           <div class="card shadow-lg p-3 mb-5 rounded">
+               <div class="container">
+                  <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-4">
+                       <p class="text-muted text-center">Genera una nueva notificación</p>
+                    </div>
+                  </div>
+                  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                  <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-2">
+                      <select class="custom-select" name="user2">
+                         <option selected>Selecciona al Usuario a Notificar</option>
+                         <?php while($row = $dato->fetch_assoc()) {  ?>
+                          <option value="<?php echo $row['Id_Usuarios']; ?>"><?php echo $row['Nombre']; echo "&nbsp;".$row['ApellidoP']; echo "&nbsp;".$row['ApellidoM']; ?></option>
+                         <?php } ?>
+                      </select>
+                    </div>
+
+                    </div>
+                    <div class="row py-2">
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-2">
+                      <select class="custom-select" name="Importancia">
+                         <option selected>Selecciona Nivel de Importancia</option>
+                         <option value="Alta">Alta</option>
+                         <option value="Media">Media</option>
+                         <option value="Baja">Baja</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row py-3">
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-2 text-center">
+                       <textarea name="Mansaje" rows="8" cols="80" class="form-control caja" placeholder="Mensaje de Notificación:"></textarea>
+                    </div>
+                  </div>
+                  <div class="row py-3">
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-4">
+                      <input type="submit" name="notificar" value="Notificar" class="btn btn-outline-success btn-sm btn-block">
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-12 py-1">
+                      <input type="submit" name="borrar" value="Borrar" class="btn btn-outline-danger btn-sm btn-block">
+                    </div>
+                  </div>
+                  <div class="row">
+                      <div class="col-sm-12 col-md-12 col-lg-12">
+                          <?php echo $alerta; ?>
+                      </div>
+                  </div>
+               </div>
+               </form>
            </div>
          </div>
-         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-         <div class="row">
-           <div class="col-sm-6 col-md-6 col-lg-6 py-4">
-             <select class="custom-select" name="user2">
-                <option selected>Selecciona al Usuario a Notificar</option>
-                <?php while($row = $dato->fetch_assoc()) {  ?>
-                 <option value="<?php echo $row['Id_Usuarios']; ?>"><?php echo $row['Nombre']; echo "&nbsp;".$row['ApellidoP']; echo "&nbsp;".$row['ApellidoM']; ?></option>
-                <?php } ?>
-             </select>
-           </div>
-           <div class="col-sm-6 col-md-6 col-lg-6 py-4">
-             <select class="custom-select" name="Importancia">
-                <option selected>Selecciona Nivel de Importancia</option>
-                <option value="Alta">Alta</option>
-                <option value="Media">Media</option>
-                <option value="Baja">Baja</option>
-             </select>
-           </div>
-         </div>
-         <div class="row py-3">
-           <div class="col-sm-12 col-md-12 col-lg-12 py-2 text-center">
-              <textarea name="Mansaje" rows="8" cols="80" class="form-control caja" placeholder="Mensaje de Notificación:"></textarea>
-           </div>
-         </div>
-         <div class="row py-3">
-           <div class="col-sm-12 col-md-12 col-lg-12 py-4">
-             <input type="submit" name="notificar" value="Notificar" class="btn btn-outline-success btn-sm btn-block">
-           </div>
-           <div class="col-sm-12 col-md-12 col-lg-12 py-1">
-             <input type="submit" name="borrar" value="Borrar" class="btn btn-outline-danger btn-sm btn-block">
-           </div>
-         </div>
-         <div class="row">
-             <div class="col-sm-12 col-md-12 col-lg-12">
-                 <?php echo $alerta; ?>
-             </div>
-         </div>
-      </div>
-      </form>
-  </div>
+     </div>
 </div>
