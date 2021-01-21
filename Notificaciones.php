d0c7c00bb18a19e6478de3c74f60345e56227bb3
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-01-21 10:50:00 -0600

    Busqueda alumno
    se completo el modulo de busqueda de el alumno perteneciente al 4 spring

--- Notificaciones.php
+++ Notificaciones.php
@@ -17,7 +17,7 @@ if ($dupla > 0) {
 // validacion de expirar sesion por tiempo
 if (isset($_SESSION['time'])) {
    // damos el timepo en segundo para determinar cuando expira la sesion
-   $inactivo = 300; // 5 minutos
+   $inactivo = 900; // 15 minutos
    // se calcula el tiempo inactivo ene l aplicativo
    $tiempo = time() - $_SESSION['time'];
    // verificamos si el tiempo pasa lo establecido para cerrar la sesion y redirigir
